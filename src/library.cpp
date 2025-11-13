3cb2dab9cdf718d68ddb03f65262f5c68bbe7538
PRAVEEN <prvnprks@gmail.com>
2025-11-13 15:47:11 +0530

    reading memory maps through parent process

--- src/library.cpp
+++ src/library.cpp
@@ -1,13 +1,29 @@
 #include "library.h"
+#include <fstream>
 
+static std::thread helloThread;
 
+std::ifstream mem_maps;
 
 void hello() {
-    // BleedLua *lua = new BleedLua();
-    // lua->execString(R"(
-    //         print("lua Test")
-    //     )");
-    // delete lua;
+    BleedLua *lua = new BleedLua();
+    lua->execString(R"(
+            print("lua Test")
+
+        )");
+    delete lua;
+
+    sleep(10);
+
+    std::cout << "test in new thread" << std::endl;
+
+    std::stringstream mapsstream;
+    mapsstream << mem_maps.rdbuf();
+
+    std::cout << mapsstream.str() << std::endl;
+
+
+
 
     BleedStream *stream = new BleedStream();
     printf("hello world stdout1\n");
@@ -15,6 +31,7 @@ void hello() {
         printf("server error\n");
     }
     stream->serverLoop();
+    delete stream;
 
 
     while (true) {
@@ -22,12 +39,31 @@ void hello() {
         sleep(1);
     }
 
+    mem_maps.close();
+
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     start_logger("BLEED: ");
 
-    std::thread helloThread(&hello);
-    helloThread.detach();
+    mem_maps = std::ifstream("/proc/self/maps");
+
+
+
+    if (mem_maps.is_open()) {
+        printf("Successfully accessed /proc/self/");
+    } else {
+        printf("Failed to access /proc/self/");
+    }
+
+    pid_t current_pid = getpid();
+    printf("The PID of the current process is: %d\n", current_pid);
+
+    helloThread = std::thread(&hello);
+
     return JNI_VERSION_1_4;
+}
+
+void JNI_OnUnload(JavaVM *vm, void *reserved) {
+    if (helloThread.joinable()) helloThread.join();
 }
