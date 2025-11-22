28d621c4e815dc35edc58004eeccde49c9a35847
PRAVEEN <prvnprks@gmail.com>
2025-11-22 23:30:53 +0530

    updated for non rooted devices

--- build_all.bat
+++ build_all.bat
@@ -1,13 +1,17 @@
 @echo off
-
-
 set NDK_PATH=D:/toolchains/android-ndk-r27d-windows/android-ndk-r27d
 
 
+if not exist build mkdir build
+cd build
+
+
 echo [Building arm64-v8a]
-mkdir build64
-cd build64
-cmake .. -G Ninja ^
+if not exist arm64-v8a mkdir arm64-v8a
+cd arm64-v8a
+
+
+cmake ../.. -G Ninja ^
     -DCMAKE_SYSTEM_NAME=Android ^
     -DCMAKE_SYSTEM_VERSION=21 ^
     -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a ^
@@ -15,14 +19,17 @@ cmake .. -G Ninja ^
     -DCMAKE_ANDROID_STL_TYPE=c++_static
 
 cmake --build .
+
 cd ..
+
+
+
 echo.
-
-
 echo [Building armeabi-v7a]
-mkdir build32
-cd build32
-cmake .. -G Ninja ^
+if not exist armeabi-v7a mkdir armeabi-v7a
+cd armeabi-v7a
+
+cmake ../.. -G Ninja ^
     -DCMAKE_SYSTEM_NAME=Android ^
     -DCMAKE_SYSTEM_VERSION=21 ^
     -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a ^
@@ -30,6 +37,7 @@ cmake .. -G Ninja ^
     -DCMAKE_ANDROID_STL_TYPE=c++_static
 
 cmake --build .
+
 cd ..
 
 echo [Done]
